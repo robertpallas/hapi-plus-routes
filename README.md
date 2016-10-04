fc142374477aa61bc23ca634d0ca5186c7e408d1
Robert <robert@devtailor.com>
2016-10-04 15:49:41 +0300

    First version

---
+++ README.md
@@ -0,0 +1,8 @@
+hapi-plus-routes
+================
+
+Hapi plugin. Automatically add routes from directory to Hapi API.
+
+This module works and is used in production but for wider usage TODO-s:
+ - documentation
+ - tests
---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const _ = require('lodash');
+const glob = require('glob');
+const Boom = require('boom');
+const Joi = require('joi');
+const chalk = require('chalk');
+
+let defaultRoute = {
+    method: 'GET',
+    handler: (request, reply) => {
+        reply(Boom.notImplemented());
+    },
+    config: {
+        auth: false,
+        plugins: {
+            policies: []
+        },
+        tags: ['api']
+    }
+};
+
+exports.register = (server, options, next) => {
+    let globOptions = {
+        nodir: true,
+        strict: true,
+        cwd: options.cwd || process.cwd()
+    };
+
+    let files = glob.sync(options.routes, globOptions);
+    files.forEach(file => {
+        let route = null;
+
+        try {
+            route = require(globOptions.cwd + '/' + file);
+            route = _.defaultsDeep(route, defaultRoute);
+
+            if (route.config.auth) {
+                route.config.validate = {
+                    headers: Joi.object({
+                        Authorization: Joi.string().description('JWT token')
+                    }).unknown()
+                };
+            }
+
+            server.route(route);
+            server.log(['startup', 'route-load'], chalk.green(route.method) + ' ' + route.path);
+        } catch (err) {
+            if (route) {
+                err = chalk.red(route.method) + ' ' + route.path + ' ' + err;
+            } else {
+                err = chalk.red(err);
+            }
+            server.log(['startup', 'route-load', 'error'], err);
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    multiple: false,
+    pkg: require('./package.json')
+};
