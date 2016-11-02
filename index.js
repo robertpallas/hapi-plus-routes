95a580749cdaa106061cc390210c7da15c43dd3b
Robert <robert@devtailor.com>
2016-11-02 19:17:57 +0200

    fix: adding auth header not overriding other validations

--- index.js
+++ index.js
@@ -35,12 +35,17 @@ exports.register = (server, options, next) => {
             route = require(globOptions.cwd + '/' + file);
             route = _.defaultsDeep(route, defaultRoute);
 
-            if (route.config.auth) {
-                route.config.validate = {
-                    headers: Joi.object({
+            if(route.config.auth) {
+                if(!route.config) {
+                    route.config = {};
+                }
+                if(!route.config.validate) {
+                    route.config.validate = {};
+                }
+                
+                route.config.validate.headers = Joi.object({
                         Authorization: Joi.string().description('JWT token')
-                    }).unknown()
-                };
+                    }).unknown();
             }
 
             server.route(route);
