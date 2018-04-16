823521743f5ac01e939cbe2eec1b7b31241b63f1
Robert Pallas <robert@devtailor.com>
2018-04-16 19:04:47 +0300

    Hapi-17: eslint airbnb default, no overrides

--- index.js
+++ index.js
@@ -6,62 +6,62 @@ const chalk = require('chalk');
 const pkg = require('./package.json');
 
 function resolveRouteImport(path) {
-    /* eslint global-require: 0, import/no-dynamic-require: 0, no-underscore-dangle: 0  */
-    const route = require(path);
-    return route.__esModule && Object.keys(route).indexOf('default') >= 0 ? route.default : route;
+  /* eslint global-require: 0, import/no-dynamic-require: 0, no-underscore-dangle: 0  */
+  const route = require(path);
+  return route.__esModule && Object.keys(route).indexOf('default') >= 0 ? route.default : route;
 }
 
 const defaultRoute = {
-    method: 'GET',
-    handler: (request, reply) => {
-        reply(Boom.notImplemented());
+  method: 'GET',
+  handler: (request, reply) => {
+    reply(Boom.notImplemented());
+  },
+  config: {
+    auth: false,
+    plugins: {
+      policies: [],
     },
-    config: {
-        auth: false,
-        plugins: {
-            policies: [],
-        },
-        tags: ['api'],
-    },
+    tags: ['api'],
+  },
 };
 
 module.exports = {
-    pkg,
-    async register(server, options) {
-        const globOptions = {
-            nodir: true,
-            strict: true,
-            cwd: options.cwd || process.cwd(),
-        };
+  pkg,
+  async register(server, options) {
+    const globOptions = {
+      nodir: true,
+      strict: true,
+      cwd: options.cwd || process.cwd(),
+    };
 
-        const files = glob.sync(options.routes, globOptions);
-        files.forEach((file) => {
-            let route = null;
+    const files = glob.sync(options.routes, globOptions);
+    files.forEach((file) => {
+      let route = null;
 
-            try {
-                route = resolveRouteImport(`${globOptions.cwd}/${file}`);
-                route = _.defaultsDeep(route, options.defaultRoute, defaultRoute);
+      try {
+        route = resolveRouteImport(`${globOptions.cwd}/${file}`);
+        route = _.defaultsDeep(route, options.defaultRoute, defaultRoute);
 
-                if (route.config.auth && !(route.config.validate && route.config.validate.headers)) {
-                    route.config.validate = route.config.validate || {};
-                    route.config.validate.headers = Joi.object({
-                        Authorization: Joi.string().description('Auth token'),
-                    }).unknown();
-                }
+        if (route.config.auth && !(route.config.validate && route.config.validate.headers)) {
+          route.config.validate = route.config.validate || {};
+          route.config.validate.headers = Joi.object({
+            Authorization: Joi.string().description('Auth token'),
+          }).unknown();
+        }
 
-                if (route.handler && route.path && route.method) {
-                    server.route(route);
-                    server.log(['startup', 'route-load'], `${chalk.green(route.method)} ${route.path}`);
-                }
-            } catch (err) {
-                let logErr;
-                if (route) {
-                    logErr = `${chalk.red(route.method)} ${route.path} ${err}`;
-                } else {
-                    logErr = chalk.red(err);
-                }
-                server.log(['startup', 'route-load', 'error'], logErr);
-            }
-        });
-    },
+        if (route.handler && route.path && route.method) {
+          server.route(route);
+          server.log(['startup', 'route-load'], `${chalk.green(route.method)} ${route.path}`);
+        }
+      } catch (err) {
+        let logErr;
+        if (route) {
+          logErr = `${chalk.red(route.method)} ${route.path} ${err}`;
+        } else {
+          logErr = chalk.red(err);
+        }
+        server.log(['startup', 'route-load', 'error'], logErr);
+      }
+    });
+  },
 };
