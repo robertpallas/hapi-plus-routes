4b75878937bb213b865945c02d67191edfef88b6
Robert Pallas <robert@devtailor.com>
2018-04-16 19:01:25 +0300

    Hapi17: match Hapi 17 plugin registration signature
    Remove tests and features related to returning as Hapi reply->return is now also default. Handling promise errors in this plugin are deprecated, all routes should do it by themselves.

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const glob = require('glob');
 const Boom = require('boom');
 const Joi = require('joi');
 const chalk = require('chalk');
-
-function getParamCount(func) {
-    return func.length;
-}
+const pkg = require('./package.json');
 
 function resolveRouteImport(path) {
-    /* eslint-disable import/no-dynamic-require, no-underscore-dangle */
+    /* eslint global-require: 0, import/no-dynamic-require: 0, no-underscore-dangle: 0  */
     const route = require(path);
     return route.__esModule && Object.keys(route).indexOf('default') >= 0 ? route.default : route;
 }
@@ -22,82 +19,49 @@ const defaultRoute = {
     config: {
         auth: false,
         plugins: {
-            policies: []
+            policies: [],
         },
-        tags: ['api']
-    }
+        tags: ['api'],
+    },
 };
 
-exports.register = (server, options, next) => {
-    const globOptions = {
-        nodir: true,
-        strict: true,
-        cwd: options.cwd || process.cwd()
-    };
+module.exports = {
+    pkg,
+    async register(server, options) {
+        const globOptions = {
+            nodir: true,
+            strict: true,
+            cwd: options.cwd || process.cwd(),
+        };
 
-    const files = glob.sync(options.routes, globOptions);
-    files.forEach((file) => {
-        let route = null;
+        const files = glob.sync(options.routes, globOptions);
+        files.forEach((file) => {
+            let route = null;
 
-        try {
-            route = resolveRouteImport(`${globOptions.cwd}/${file}`);
-            route = _.defaultsDeep(route, options.defaultRoute, defaultRoute);
+            try {
+                route = resolveRouteImport(`${globOptions.cwd}/${file}`);
+                route = _.defaultsDeep(route, options.defaultRoute, defaultRoute);
 
-            if(route.config.auth && !(route.config.validate && route.config.validate.headers)) {
-                route.config.validate = route.config.validate || {};
-                route.config.validate.headers = Joi.object({
-                    Authorization: Joi.string().description('Auth token')
-                }).unknown();
-            }
-
-            if(route.handler) {
-                if(getParamCount(route.handler) < 2) {
-                    // default signature is (request, reply)
-                    // if reply is omitted, then the return value should be interpreted
-                    // as the reply.
-                    // we wrap the handler function to support this
-                    const orignalHandler = route.handler;
-                    route.handler = (request, reply) => {
-                        const retValue = orignalHandler.call({}, request);
-                        if(retValue instanceof Promise) {
-                            return retValue
-                          .then(reply)
-                          .catch((err) => {
-                              // handle promise rejection
-                              if(err && err.isBoom) {
-                                  // boom errors are put through
-                                  return reply(err);
-                              }
-                                  // other errors are logged and a generic error is sent instead
-                              server.log([request.method, request.path, 'error'], err);
-                              return reply(Boom.internal());
-                          });
-                        }
-
-                        return reply(retValue);
-                    };
+                if (route.config.auth && !(route.config.validate && route.config.validate.headers)) {
+                    route.config.validate = route.config.validate || {};
+                    route.config.validate.headers = Joi.object({
+                        Authorization: Joi.string().description('Auth token'),
+                    }).unknown();
                 }
-            }
 
-            if(route.handler && route.path && route.method) {
-                server.route(route);
-                server.log(['startup', 'route-load'], `${chalk.green(route.method)} ${route.path}`);
+                if (route.handler && route.path && route.method) {
+                    server.route(route);
+                    server.log(['startup', 'route-load'], `${chalk.green(route.method)} ${route.path}`);
+                }
+            } catch (err) {
+                let logErr;
+                if (route) {
+                    logErr = `${chalk.red(route.method)} ${route.path} ${err}`;
+                } else {
+                    logErr = chalk.red(err);
+                }
+                server.log(['startup', 'route-load', 'error'], logErr);
             }
-        } catch(err) {
-            let logErr;
-            if(route) {
-                logErr = `${chalk.red(route.method)} ${route.path} ${err}`;
-            } else {
-                logErr = chalk.red(err);
-            }
-            server.log(['startup', 'route-load', 'error'], logErr);
-        }
-    });
-
-    next();
-};
-
-exports.register.attributes = {
-    multiple: false,
-    pkg: require('./package.json')
+        });
+    },
 };
--- test/routes/users/promiseMeRejection.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    path: '/users/promisemerejection',
-    method: 'GET',
-    handler: request => Promise.reject(new Error('My exception')),
-    config: {
-        auth: 'jwt',
-        description: 'Get current user details'
-    }
-};
